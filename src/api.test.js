8bfb4b93bad4dddafe2cd67d673d01e858969d6c
Steinar Sigurdsson <Steinar@Steinars-MacBook-Pro.local>
2018-10-10 16:12:10 +0000

    jaj

--- src/api.test.js
+++ src/api.test.js
@@ -3,7 +3,7 @@ const api = require("./api");
 describe("GET /greeting/:name endpoint", () => {
     it("should return a 200 OK status code", async () => {
         const res = await request(api).get("/greeting/_");
-        expected(res.status).toBe(200);
+        expect(res.status).toBe(200);
     });
     it("should return the greeting in a object", async () => {
         const res = await request(api).get("/greeting/Steinar");
